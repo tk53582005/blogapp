1bd38d93652b26661b2a1cd984fd69b27e85fef9
tk53582005 <tk53582005@yahoo.co.jp>
2025-11-15 16:36:48 +0900

    Add comment feature with user association

---
+++ test/models/comment_test.rb
@@ -0,0 +1,27 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :integer          not null, primary key
+#  content    :text             not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  article_id :integer          not null
+#  user_id    :integer          not null
+#
+# Indexes
+#
+#  index_comments_on_article_id  (article_id)
+#  index_comments_on_user_id     (user_id)
+#
+# Foreign Keys
+#
+#  user_id  (user_id => users.id)
+#
+require "test_helper"
+
+class CommentTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
