1db9cba7f9485101e753f795d1968c862947fdef
tk53582005 <tk53582005@yahoo.co.jp>
2025-12-09 22:34:12 +0900

    フォロー機能とタイムライン機能を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@
 #
 # Table name: users
 #
-#  id                     :integer          not null, primary key
+#  id                     :bigint           not null, primary key
 #  email                  :string           default(""), not null
 #  encrypted_password     :string           default(""), not null
 #  remember_created_at    :datetime
@@ -25,6 +25,13 @@ class User < ApplicationRecord
   has_many :articles, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :favorite_articles, through: :likes, source: :article
+
+  has_many :following_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followings, through: :following_relationships, source: :following
+
+  has_many :follower_relationships, foreign_key: 'following_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followers, through: :follower_relationships, source: :follower
+
   has_one :profile, dependent: :destroy
 
   delegate :birthday, :age, :gender, to: :profile, allow_nil: true
@@ -35,22 +42,34 @@ class User < ApplicationRecord
 
   def has_liked?(article)
     likes.exists?(article_id: article.id)
-
   end
 
-  def display_name
-    profile&.nickname || self.email.split('@').first
+  def follow!(user)
+    user_id = get_user_id(user)
+    following_relationships.create!(following_id: user_id)
+  end
+
+  def unfollow!(user)
+    user_id = get_user_id(user)
+    relation = following_relationships.find_by!(following_id: user_id)
+    relation.destroy!
+  end
+
+  def has_followed?(user)
+    user_id = get_user_id(user)
+    following_relationships.exists?(following_id: user_id)
   end
 
   def prepare_profile
     profile || build_profile
   end
 
-  def avatar_image
-    if profile&.avatar&.attached?
-      profile.avatar
+  private
+  def get_user_id(user)
+    if user.is_a?(User)
+      user.id
     else
-      'default-avatar.png'
+      user
     end
   end
 end
